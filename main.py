757c7ea3ae47e73a895d18f6feb9ccddcf7ecfe9
larry3425527 <larrye59678991s@gmail.com>
2021-10-03 21:50:03 +0800

    123

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 import time
+import requests
+import urllib.parse
+import json
 from typing import Sized
 from flask import Flask
 from flask import render_template
+from requests.api import get
 from UserData.AddressData import Taipei
 from src.spider import spider
 from src.dataType import houseInfoType
-
 app = Flask(__name__, static_url_path='/static')
 spi = spider()
 lastUpdateTime = ''
 l = []
 Hospital = []
-##醫院資料
-temp = Taipei().hospital()
-for i in range(0,len(temp)):
-    Hospital.append(temp[i].address)
-##充電站
-ChargeStation = Taipei().EF()
-##社區大學
-College = Taipei().college()
+HospLatLng = []
+
+def get_latitude_longtitude(address,list,name):
+    # decode url
+    address = urllib.parse.quote(address)
+    url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address +"&key=AIzaSyAui41PhiDPXFDiZcWUbp3h6mMCJ5Bjx6Q"
+    while True:
+        res = requests.get(url)
+        js = json.loads(res.text)
+
+        if js["status"] != "OVER_QUERY_LIMIT":
+            time.sleep(1)
+            break
+    result = js["results"][0]["geometry"]["location"]
+    temp = {'name': name , 'lat': float(result["lat"]), 'lng': float(result["lng"])}
+    list.append(temp)
+    
 
 
 def initData():
@@ -29,6 +41,19 @@ def initData():
             l.extend([houseInfoType(x[:-1].split(',')) for x in f.readlines()])
     except Exception as e:
         print(e)
+
+##醫院資料
+temp = Taipei().hospital()
+for i in range(0,len(temp)):
+    Hospital.append(temp[i].address)
+    get_latitude_longtitude(Hospital[0],HospLatLng,temp[i].name)
+    print(HospLatLng[i] , '\n')
+    
+##充電站
+ChargeStation = Taipei().EF()
+##社區大學
+College = Taipei().college()
+
 
 @app.route('/')
 def home():
